e162e67fbee621b626cfa3d443133a4a4216ee16
kevon2019 <43534903+kevon2019@users.noreply.github.com>
2022-03-20 21:17:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ export default {
   githubName: 'kevon2019',
   githubRepo: 'nuxt2-blog',
   githubBranch: 'master',
-  domain: 'https://blog.halberd.cn',
-  SEO_description: '云与原的博客，书写值得书写的东西',
-  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,云与原的博客',
+  domain: 'http://kwf10583.gicp.net',
+  SEO_description: '己所不欲勿施于人',
+  SEO_keywords: 'kevon\'s blog,己所不欲勿施于人',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc"
 }
