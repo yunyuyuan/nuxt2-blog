ed5ed9f22785382aba3238f421687d701a67aaae
kevon2019 <43534903+kevon2019@users.noreply.github.com>
2022-03-20 23:31:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export default {
-  title: 'kevon',
+  title: 'kevon自留地',
   githubName: 'kevon2019',
   githubRepo: 'nuxt2-blog',
   githubBranch: 'master',
   domain: 'http://kwf10583.gicp.net',
-  SEO_description: '己所不欲勿施于人',
-  SEO_keywords: 'kevon\'s blog,己所不欲勿施于人',
+  SEO_description: '',
+  SEO_keywords: 'kevon\'s blog,',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc"
 }
