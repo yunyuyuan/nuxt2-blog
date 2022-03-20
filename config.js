b763e87d8970a0938aa85b238eddd976692f8008
kevon2019 <43534903+kevon2019@users.noreply.github.com>
2022-03-20 20:48:44 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export default {
   title: 'yun yu yuan',
-  githubName: 'yunyuyuan',
+  githubName: 'kevon2019',
   githubRepo: 'nuxt2-blog',
   githubBranch: 'master',
   domain: 'https://blog.halberd.cn',
